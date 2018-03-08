2cf2af433bee537eae870c3ea3e826e2cef81204
Morten Norgaard <morten@mortennorgaard.dk>
2018-03-08 09:53:39 +0100

    Musings on composite design pattern.

--- DesignPatterns/StructuralPatterns/CompositePattern/CompositePattern_example/CompositePatern_challenge/Program.cs
+++ DesignPatterns/StructuralPatterns/CompositePattern/CompositePattern_example/CompositePatern_challenge/Program.cs
@@ -10,6 +10,16 @@ namespace CompositePatern_challenge
     {
         private static void Main(string[] args)
         {
+
+
+            throw new NotImplementedException(
+                "Consider using the SKAT example of IRpaProcessUnitComponent - to represent unknown" +
+                "hierarchy levels on rpa-identification/-extraction jobs. Just need some method to call... easy to fake on (write to db, or some such");
+
+
+            //TODO: do _remember_ to promote the execute-as-group abstraction - or it would make more sense to just add a reference-to-self variable....?
+
+
         }
     }
 }
