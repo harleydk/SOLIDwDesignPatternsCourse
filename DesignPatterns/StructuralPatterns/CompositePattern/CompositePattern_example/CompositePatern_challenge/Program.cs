c6ef382e893ce4a0e1e1e3422c0a78b3bbb4223d
Morten Nørgaard <W23043adm@ccta.dk>
2018-07-13 08:02:57 +0200

    documentation

--- DesignPatterns/StructuralPatterns/CompositePattern/CompositePattern_example/CompositePatern_challenge/Program.cs
+++ DesignPatterns/StructuralPatterns/CompositePattern/CompositePattern_example/CompositePatern_challenge/Program.cs
@@ -10,9 +10,7 @@ namespace CompositePatern_challenge
     {
         private static void Main(string[] args)
         {
-
-
-            throw new NotImplementedException(
+            throw new NeverGotAroundToImplementingThisException(
                 "Consider using the SKAT example of IRpaProcessUnitComponent - to represent unknown" +
                 "hierarchy levels on rpa-identification/-extraction jobs. Just need some method to call... easy to fake on (write to db, or some such");
 
@@ -20,6 +18,13 @@ namespace CompositePatern_challenge
             //TODO: do _remember_ to promote the execute-as-group abstraction - or it would make more sense to just add a reference-to-self variable....?
 
 
+        }
+
+        private class NeverGotAroundToImplementingThisException : Exception
+        {
+            public NeverGotAroundToImplementingThisException(string message) : base(message)
+            {
+            }
         }
     }
 }
